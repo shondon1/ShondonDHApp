86c11ca8561da50cb2c2c4de03613160811cf9c8
Rashon <rashyslop@outlook.com>
2025-06-21 18:22:39 -0600

    Initial Commit

---
+++ ShondonDHApp/ShondonDHAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShondonDHAppApp.swift
+//  ShondonDHApp
+//
+//  Created by Rashon Hyslop on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ShondonDHAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
